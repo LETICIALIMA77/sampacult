e6bc474a74dae372e9bc9177c0726870d44ac891
AnaLimeira26 <113556397+AnaLimeira26@users.noreply.github.com>
2022-10-25 17:02:10 -0300

    Update eventos.php

--- eventos.php
+++ eventos.php
@@ -14,8 +14,8 @@
     <link rel="stylesheet" type="text/css" href="./asset/css/plugins/simple-line-icons.css" />
     <link rel="stylesheet" type="text/css" href="./asset/css/plugins/animate.min.css" />
     <link rel="stylesheet" type="text/css" href="./asset/css/plugins/fullcalendar.min.css" />
-    <link href="./style.css" rel="stylesheet">
-    <link href="./index.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
+    <link href="./css/index.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css" integrity="sha384-X8QTME3FCg1DLb58++lPvsjbQoCT9bp3MsUU3grbIny/3ZwUJkRNO8NPW6zqzuW9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -38,8 +38,8 @@
     <div class="dashboard-nav">
         <nav class="dashboard-nav-list active">
             <header style="background-color:#fbf4dc">
-                <a href="./cult.jpeg" class="brand-logo">
-                    <img src="./cult.jpeg" style="width:200px; height:130px; padding:05px" alt="">
+                <a href="./imagens/cult.jpeg" class="brand-logo">
+                    <img src="./imagens/cult.jpeg" style="width:200px; height:130px; padding:05px" alt="">
                 </a>
             </header>
             <li class="active" style="background-color:#fbf4dc">
@@ -73,7 +73,7 @@
                 <i class="bi bi-stars"></i>
                     EVENTOS
                 </a>
-                    <a href="./sobre.php" class="dashboard-nav-item " style="color:black">
+                    <a href="#" class="dashboard-nav-item " style="color:black">
                     <!-- <i class="fa-solid fa-triangle-exclamation fa-fade"></i> -->
                     <i class="bi bi-journals"></i>   
                     SOBRE
@@ -116,24 +116,24 @@
         </nav>
         <div class="container-eventos">
  <h3 class="titulo-eventos" style="font-size:xx-large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">AGOSTO BRANCO</h3>
-                <p class="paragrafo-eventos">
+                <p class="paragrafo-eventos"style= "padding:20px;">
                 A nossa apresentação sobre o Agosto Branco foi apresentada pela Marcella e pela Ana Paula. 
 Elas começaram explicando o que significa a campanha do Agosto Branco, como ela surgiu e a sua importância.
 Logo em seguida elas falaram alguns sintomas da doença, como tentar prevenir o câncer e o que faz aumentar o risco de desenvolver essa doença. 
 
                 </p>
 <h3 class="titulo-eventos2" style="font-size:large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">O que é o Agosto Branco</h3>
-<p class="paragrafo-eventos2">
+<p class="paragrafo-eventos2" style= "padding:20px;">
 É uma campanha que busca espalhar informações sobre os perigos do câncer de pulmão.
 A ideia principal é conscientizar as pessoas sobre as causas, para poderem se prevenir, e também terem consciência dos sintomas, para que o diagnóstico seja feito e o tratamento possa ser realizado o mais rápido possível. 
   </p>
   <h3 class="titulo-eventos3" style="font-size:large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Como surgiu?</h3>
-  <p class="paragrafo-eventos3">
+  <p class="paragrafo-eventos3" style= "padding:20px;">
   Surgiu para a conscientização da população, já que esse é o câncer que tem a maior taxa de mortalidade no país. 
 Sendo a principal em homens e a segunda em mulheres.
   </p>
   <h4 class="titulo-eventos4" style="font-size:large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Sintomas</h4>
-  <p class="paragrafo-eventos4">
+  <p class="paragrafo-eventos4" style= "padding:20px;">
 •	Tosse persistente;<br>
 •	Tosse com sangue;<br>
 •	Dor no peito;<br>
@@ -144,14 +144,14 @@ Sendo a principal em homens e a segunda em mulheres.
 
   </p>
   <h4 class="titulo-eventos5" style="font-size:large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Prevenção</h4>
-  <p class="paragrafo-eventos5">
+  <p class="paragrafo-eventos5" style= "padding:20px;">
   •	Não fumar; <br>
 •	Evitar o tabagismo passivo.
   </p><br><br>
-  <img src="./foto-eventos.png" style="width:600px; height:300px;" alt="">
+  <img src="./imagens/foto-eventos.png" style="width:600px; height:300px;" alt="">
 <br><br>
 <h4 class="titulo-eventos6" style="font-size:XX-LARGE; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">DIA DO FOLCLORE</h4>
-  <p class="paragrafo-eventos6">
+  <p class="paragrafo-eventos6" style= "padding:20px;">
   A Apresentação sobre o Dia Do Folclore foi feita pela Julia, pela Marcella e pela Nicoly. 
 As meninas começaram explicando em que dia que se comemora o dia do Folclore e porque ele é comemorado.
 Elas falaram sobre a importância do Folclore e os personagens mais lembrados do nosso Folclore.
@@ -162,18 +162,18 @@ O Dia do Folclore, comemorado em 22 de agosto, foi criado com o objetivo de ince
 No Brasil, o Dia do Folclore foi oficializado em 17 de agosto de 1965, por meio do Decreto nº 56.747.
   </p>
   <h4 class="titulo-eventos7" style="font-size:large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Importância do Folclore Brasileiro</h4>
-  <p class="paragrafo-eventos7"> 
+  <p class="paragrafo-eventos7"style= "padding:20px;"> 
   Atualmente, o folclore brasileiro é um importante objeto de estudo nas ciências humanas, e sua importância é reforçada frequentemente nas escolas, sobretudo naquelas que trabalham com ensino infantil.
 No Dia do Folclore, costuma-se relembrar os elementos mais importante da cultura popular brasileira, tais como as danças, os ritmos, as festas e os personagens do nosso folclore. Nas danças e ritmos, podem ser citados o frevo, o maracatu, o baião, o forró, a catira etc. 
   </p>
   <h4 class="titulo-eventos8" style="font-size:large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Figuras do Nosso Folclore</h4>
-  <p class="paragrafo-eventos8">Entre os personagens, estão inseridas as lendas tradicionais de nosso país. As principais lendas são a do saci e do curupira, mas outros personagens importantes do nosso folclore são: a Iara, o boto cor-de-rosa, a mula sem cabeça, o boitatá, o negrinho do pastoreio, entre outros. </p>
-  <img src="./foto-eventos2.png" style="width:600px; height:300px;" alt="">
+  <p class="paragrafo-eventos8" style= "padding:20px;">Entre os personagens, estão inseridas as lendas tradicionais de nosso país. As principais lendas são a do saci e do curupira, mas outros personagens importantes do nosso folclore são: a Iara, o boto cor-de-rosa, a mula sem cabeça, o boitatá, o negrinho do pastoreio, entre outros. </p>
+  <img src="./imagens/foto-eventos2.png" style="width:600px; height:300px;" alt="">
   <br><br><br>
-  <img src="./foto-eventos3.png" style="width:600px; height:300px;" alt="">
+  <img src="./imagens/foto-eventos3.png" style="width:600px; height:300px;" alt="">
   <br><br><br>
   <h4 class="titulo-eventos8" style="font-size:xx-large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Dia Nacional das Artes</h4>
-  <p class="paragrafo-eventos8"> A apresentação do Dia Nacional das Artes foi apresentada pela Ana Paula e pela Rayssa.
+  <p class="paragrafo-eventos8" style= "padding:20px;"> A apresentação do Dia Nacional das Artes foi apresentada pela Ana Paula e pela Rayssa.
 As meninas falaram que dia que se comemora o dia nacional das artes, porque ela é celebrada nesse dia.
 Depois elas explicaram o porquê dessa existe o dia Nacional das Artes, as meninas finalizaram dando dicas de lugares em São Paulo para se visitar e apreciar as Artes.
 <br><br>
@@ -181,7 +181,7 @@ Depois elas explicaram o porquê dessa existe o dia Nacional das Artes, as menin
 E da regulamentação da profissão de Artista e Técnico em Espetáculos de Diversões, através da lei <strong> nº 6.533, de 24 de maio de 1978 e do decreto lei nº 82.385, de 5 de outubro de 1978.</strong>
 </p>
 <h4 class="titulo-eventos9" style="font-size:large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Celebração</h4>
-<p class="paragrafo-eventos9">A ideia deste dia é celebrar as mais diversas expressões artísticas, como a música, a dança, a literatura, o teatro, o circo, entre outras. 
+<p class="paragrafo-eventos9" style= "padding:20px;">A ideia deste dia é celebrar as mais diversas expressões artísticas, como a música, a dança, a literatura, o teatro, o circo, entre outras. 
 Fora reforça a importância do reconhecimento de tais atividades culturais como fundamentais para o desenvolvimento humano.
 </p>
 <h4 class="titulo-eventos10" style="font-size:large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Onde ir nesse dia;</h4>
@@ -189,28 +189,28 @@ Fora reforça a importância do reconhecimento de tais atividades culturais como
     <strong style="font-size:20px ;">BECO DO BATMAN - VILA MADALENA</strong>
 </p>
 <br><br>
-<img src="./foto-eventos4.png" style="width:600px; height:300px;" alt="">
+<img src="./imagens/foto-eventos4.png" style="width:600px; height:300px;" alt="">
 <br><br>
 <strong style="font-size:20px;">MUSEU DE ARTE DE SÃO PAULO - MASP - AVENIDA PAULISTA</strong>
 <br><br>
-<img src="./foto-eventos5.png" style="width:600px; height:300px;" alt="">
+<img src="./imagens/foto-eventos5.png" style="width:600px; height:300px;" alt="">
 <br><br>
-<img src="./eventos6.png" style="width:600px; height:300px;" alt="">
+<img src="./imagens/eventos6.png" style="width:600px; height:300px;" alt="">
 <br><br>
-<img src="./eventos7.png" style="width:600px; height:300px;" alt="">
+<img src="./imagens/eventos7.png" style="width:600px; height:300px;" alt="">
 <h4 class="titulo-eventos11" style="font-size:xx-large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;padding:20px;">Educação Financeira
 <br><br>Canal Me Poupe!
 </h4>
-<p class="paragrafo-eventos11">Essa apresentação sobre Educação Financeira e sobre o Canal Me Poupe!, foi feita pela Carol e pela Franciele.
+<p class="paragrafo-eventos11" style= "padding:20px;">Essa apresentação sobre Educação Financeira e sobre o Canal Me Poupe!, foi feita pela Carol e pela Franciele.
 As meninas começaram explicando quem é a apresentadora e dona do Canal Me Poupe Natalia Arcuri, falando que ela é jornalista, apresentadora e especialista em finanças e que ela ensina sobre como poupar dinheiro.
 Depois elas deram 3 dicas para sobrar dinheiro no final do mês, essas dicas foram dadas pela Natalia em seu canal do YouTube Me Poupe!
 </p>
 <h4 class="titulo-eventos12" style="font-size:larger; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Quem é Nathalia Arcuri?</h4>
-<p class="paragrafo-eventos12">Nathalia Arcuri é especialista em finanças, jornalista, apresentadora e criadora do Canal Me Poupe!, o maior canal de finanças do mundo.
+<p class="paragrafo-eventos12"style= "padding:20px;">Nathalia Arcuri é especialista em finanças, jornalista, apresentadora e criadora do Canal Me Poupe!, o maior canal de finanças do mundo.
 Ela ensina sobre educação financeira, com um toque de humor e entretenimento. Desse jeito a youtuber está presente em diversas plataformas compartilhando conteúdos sobre investimentos, principalmente, em renda fixa.
 </p>
 <h4 class="titulo-eventos13" style="font-size:large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">3 Dicas Para Sobrar Dinheiro no Final do Mês </h4>
-<p class="paragrafo-eventos13">
+<p class="paragrafo-eventos13" style= "padding:20px;">
     <strong>1ª Dica: Anote seus gastos</strong>
 <br><br>
 É muito importante saber o quanto você ganha e saber no detalhe os seus custos, desde aqueles necessários e fixos como por exemplo, aluguel, conta de luz, água, etc, e até aqueles pequenos, como as compras que fazemos aqui e ali, o gasto do supermercado, os deslocamentos para irmos em determinado lugar, etc.
@@ -223,13 +223,64 @@ Existe uma lei que autoriza que lojas apliquem descontos dependendo do meio de p
 Não clique em links suspeitos, não acredite em promoções bombásticas, nem em promessas de ganhos fáceis. Tem muito golpista por aí querendo tirar o seu dinheiro, então fique alerta! 
 Uma dica é sempre acessar o site pela página oficial – não clique no link que recebeu, digite diretamente o nome da loja no seu navegador e acesse o site oficial para validar.
 </p>
-<img src="./foto-eventos6.png" style="width:600px; height:300px;" alt="">
+<img src="./imagens/foto-eventos6.png" style="width:600px; height:300px;" alt="">
 <br><br><br>
-<img src="./foto-eventos7.png" style="width:600px; height:300px;" alt="">
+<img src="./imagens/foto-eventos7.png" style="width:600px; height:300px;" alt="">
 <br><br><br><br><br>
-<img src="./foto-eventos8.png" style="width:600px; height:300px;" alt=""> 
+<img src="./imagens/foto-eventos8.png" style="width:600px; height:300px;" alt=""> 
 <h4 class="titulo-eventos14"style="font-size:xx-large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Setembro Amarelo</h4>
-<p class="paragrafo-eventos14"></p>
+<p class="paragrafo-eventos14" style= "padding:20px;"> 
+A apresentação do Setembro Amarelo foi feita pela Ana Flávia.
+Ela começou explicando o que era o Setembro Amarelo é porque é tão importante.
+Depois ela contou como surgiu o Setembro Amarelo e o significado do Amarelo.
+Logo em seguida ela falou o CVV (Centro de Valorização a Vida) e mostrou como entrar em contato com o CVV.
+No final da apresentação foi mostrado três vídeos sobre a temática.
+Quando a apresentação acabou algumas pessoas contaram sobre situações que já viveram ou já presenciaram com algum conhecido. <br> <br>
+
+<strong>Setembro Amarelo</strong> <br> <br>
+
+Setembro Amarelo é a campanha de prevenção ao suicídio da ABP (Associação Brasileira de Psiquiatria).
+Sendo dia 10 o Dia Mundial de Prevenção ao Suicídio. <br> <br>
+
+<strong>Como Surgiu?</strong> <br> <br>" style= "padding:20px;"
+
+O Setembro Amarelo começou nos EUA, quando o jovem Mike Emme, de 17 anos, cometeu suicídio, em 1994. Mike era um rapaz muito habilidoso e restaurou um automóvel Mustang 68, pintando-o de amarelo. Por conta disso, ficou conhecido como "Mustang Mike". <br> <br>
+
+<strong>CVV</strong> <br> <br>
+
+O CVV – Centro de Valorização da Vida realiza apoio emocional e prevenção do suicídio, atendendo voluntária e gratuitamente todas as pessoas que querem e precisam conversar, sob total sigilo por telefone, email e chat 24 horas todos os dias. <br> <br>
+
+<strong>Contato CVV</strong> <br> <br>
+
+Telefone: 188 <br>
+Chat: https://www.cvv.org.br/chat/ <br>
+Polo Físico: Jabaquara - São Paulo <br>
+Av: Jabaquara, 2607 
+Horário: 24 horas</p> <br> <br> <br>
+<img src="./imagens/sa1.jpeg" style="width:600px; height:300px;" alt=""> <br> <br> <br>
+<img src="./imagens/SA-2.jpeg" style="width:600px; height:300px;" alt=""> <br> <br> <br>
+<img src="./imagens/SA-5.jpeg" style="width:600px; height:300px;" alt=""> <br> <br> <br>
+<img src="./imagens/SA-4.jpeg" style="width:600px; height:300px;" alt=""> <br> <br> <br>
+<h4 class="titulo-eventos14"style="font-size:xx-large; font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding:20px;">Outubro Rosa</h4>
+<p class="paragrafo-eventos14" style= "padding:20px;">
+A apresentação foi feita pela Ana Flávia, ela explicou o que era o Outubro Rosa, como surgiu a campanha, sintomas do câncer de mama e mostrou dicas de auto cuidado. <br> <br>
+<strong>O que é Outubro Rosa?</strong> <br> <br>
+É uma campanha com o objetivo de compartilhar informações e promover a consciencialização sobre o câncer de mama, proporcionando maior acesso aos serviços de diagnóstico e de tratamento e contribuir para a redução da mortalidade. <br> <br>
+<strong>Como Surgiu?</strong> <br> <br>
+O Outubro Rosa apareceu pela primeira vez nos Estados Unidos, quando vários estados se uniram para realizar ações sobre o câncer de mama. <br>
+O laço rosa, símbolo usado na campanha, foi uma iniciativa da Fundação Susan G. Komen for the Cure, que distribuiu o item aos participantes da Corrida pela Cura, em Nova York, na década de 1990. <br>
+Posteriormente, hospitais começaram a aderir aos programas de divulgação sobre a doença, distribuindo os laços pela cidade. <br> <br>
+<strong> Sintomas do Câncer Mama </strong> <br> <br>
+•	A presença de caroços na região da mama; <br>
+•	Secreções no mamilo; <br>
+•	Pele enrugada e enrijecida;<br>
+•	Vermelhidão, feridas e coceiras.<br> <br>
+<strong> Auto Cuidado </strong> <br> <br>
+<img src="./imagens/5.png" style="width:600px; height:300px;" alt=""> <br> <br> <br>
+<img src="./imagens/2.png" style="width:600px; height:300px;" alt=""> <br> <br> <br>
+<img src="./imagens/3.png" style="width:600px; height:300px;" alt=""> <br> <br> <br>
+<img src="./imagens/4.png" style="width:600px; height:300px;" alt=""> <br> <br> <br>
+
 </div> 
 </div>
 </body>
