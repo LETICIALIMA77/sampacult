81b8d6b1a2111ea71e2846d39698ead33d458646
Letícia Lima <101489147+LETICIALIMA77@users.noreply.github.com>
2022-11-14 20:23:06 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAMPA CULT</title>
+    <meta name="description" content="Miminium Admin Template v.1">
+    <meta name="author" content="Isna Nur Azis">
+    <meta name="keyword" content="">
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="./asset/css/plugins/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="./asset/css/plugins/simple-line-icons.css" />
+    <link rel="stylesheet" type="text/css" href="./asset/css/plugins/animate.min.css" />
+    <link rel="stylesheet" type="text/css" href="./asset/css/plugins/fullcalendar.min.css" />
+    <link href="./css/style.css" rel="stylesheet">
+    <link href="./css/index.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css" integrity="sha384-X8QTME3FCg1DLb58++lPvsjbQoCT9bp3MsUU3grbIny/3ZwUJkRNO8NPW6zqzuW9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="path/to/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">  
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="dashboard-nav">
+        <nav class="dashboard-nav-list active">
+            <header style="background-color:#fbf4dc">
+                <a href="./img/cult.jpeg" class="brand-logo">
+                    <img src="./img/cult.jpeg" style="width:200px; height:130px; padding:05px" alt="">
+                </a>
+            </header>
+            <li class="active" style="background-color:#fbf4dc">
+                <a href="./index.php" class="dashboard-nav-item active" style="color:black">
+                    <!-- <i class="fas fa-sign-out-alt"></i> -->
+                    <i class="bi bi-bank"></i>
+                    PÁGINA INICIAL
+                </a>
+                <a href="./tecinformação.php" class="dashboard-nav-item " style="color:black">
+                <!-- <i class="fa-solid fa-cog fa-spin" style="--fa-animation-duration: 15s;"></i> -->
+                <i class="bi bi-pc-display-horizontal"></i>
+                TEC INFORMAÇÃO
+                </a>
+                <a href="./marketing.php" class="dashboard-nav-item " style="color:black">
+                <!-- <i class="fa-solid fa-money-check-dollar fa-flip" style="--fa-animation-duration: 3s;" ></i> -->
+                <i class="bi bi-twitter"></i>   
+                MARKETING
+                </a>
+                <a href="./recursoshumanos.php" class="dashboard-nav-item  " style="color:black">
+                <!-- <i class="fa-solid fa-scroll fa-flip" style="--fa-flip-x: 1; --fa-flip-y: 0;" ></i> -->
+                <i class="bi bi-wallet-fill"></i>   
+                RECURSOS HUMANOS
+                </a>
+                <a href="./administracao.php" class="dashboard-nav-item  " style="color:black">
+                <!-- <i class="fa-solid fa-cog fa-spin" style="--fa-animation-duration: 15s;"></i> -->
+                <i class="bi bi-menu-button-wide-fill"></i>   
+                ADMINISTRAÇÃO
+                </a>
+                <a href="./eventos.php" class="dashboard-nav-item  " style="color:black">
+                <!-- <i class="fa-solid fa-circle-plus fa-beat"></i> -->
+                <i class="bi bi-stars"></i>
+                    EVENTOS
+                </a>
+                    <a href="./sobre.php" class="dashboard-nav-item " style="color:black">
+                    <!-- <i class="fa-solid fa-triangle-exclamation fa-fade"></i> -->
+                    <i class="bi bi-journals"></i>   
+                    SOBRE
+                    </a>
+        
+            </li>
+        </nav>
+    </div><br>
+    <div id="content">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <button type="button" id="botao-menu" style="background-color:#a59b85; border-radius:2px;
+                 border-color:#a59b85; height:40px; width:70px">
+                    <span>Menu</span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link " href="./index.php">Página Inicial</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./tecinformacao.php">Tec Informação</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="./marketing.php">Marketing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./recursoshumanos.php">Rec Humanos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./administracao.php">Admin</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="./eventos.php">Eventos</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+</body>
+<script>
+    const mobileScreen = window.matchMedia("(max-width: 990px )");
+    $(document).ready(function() {
+        $(".dashboard-nav-dropdown-toggle").click(function() {
+            $(this).closest(".dashboard-nav-dropdown")
+                .toggleClass("show")
+                .find(".dashboard-nav-dropdown")
+                .removeClass("show");
+            $(this).parent()
+                .siblings()
+                .removeClass("show");
+        });
+        $(".menu-toggle").click(function() {
+            if (mobileScreen.matches) {
+                $(".dashboard-nav").toggleClass("mobile-show");
+            } else {
+                $(".dashboard").toggleClass("dashboard-compact");
+            }
+        });
+    });
+    $(document).ready(function() {
+        $('#botao-menu').on('click', function() {
+            $('.dashboard-nav').toggleClass('.dashboard-nav-list active');
+        });
+    });
+</script>
+
+</html>
